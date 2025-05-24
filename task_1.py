8a6efd4d03b6696ed0b9bbe626fc104df93ea544
Saryanarayana <satyanarayanaalla88@gmail.com>
2025-05-24 14:52:42 +0530

    Delete task_1.py

--- task_1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-df=pd.read_csv(r"C:\Users\satya\OneDrive\Desktop\inten\API_SP.POP.TOTL_DS2_en_csv_v2_85220.csv",skiprows=4)
-print(df.head(5))
-print(df.isnull().sum())
-
